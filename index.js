fa4e425c660e34185ee97983c1684dd58b441c2d
Sami <skalexsong@gmail.com>
2021-06-15 21:43:22 +0700

    Update : add more example as basics

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
-
+const router = require("./router");
 const PORT = 3000;
 
-// Endpoint handler
-app.get("/", (req, res) => res.render("/src/pages/index.html"));
-app.get("/games", (req, res) => res.render("/src/pages/games.html"));
+// Application level middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+app.use(logger);
+
+// Built-in middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Route level middleware
+app.use(router);
+
+app.get("/games", (req, res) =>
+  res.sendFile(path.join(__dirname + "/index.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`This app listening on http://localhost:${PORT}`)
