46137620388d8857b918aa55369b4cf1e5f0a7a4
Sami <skalexsong@gmail.com>
2021-06-24 23:33:38 +0700

    Update : fix basic routes

--- routes/games.js
+++ routes/games.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Traditional Games" });
+  res.render("games", { title: "Try Out The Games" });
 });
 
 module.exports = router;
