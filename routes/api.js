5bf5773a1dec84323d4d3865f0db0a79048f19b1
Sami <skalexsong@gmail.com>
2021-06-28 22:46:22 +0700

    Update : add method

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const users = require("../db/user.json");
+let users = require("../db/user.json");
 const app = express();
 
 app.use(express.Router());
+app.use(express.json());
 
 // Get /user
 app.get("/v1/user", (req, res, next) => res.json(users));
@@ -15,21 +16,38 @@ app.get("/v1/user/:id", (req, res, next) => {
   }
 });
 
-app.use(express.json());
-
 // Post /user
 app.post("/v1/user", (req, res, next) => {
   const { name, email, password } = req.body;
   const id = users[users.length - 1].id + 1;
-  const newUser = {
+  const user = {
     id,
     name,
     email,
     password,
   };
 
-  users.push(newUser);
-  res.status(201).json(newUser);
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.put("/v1/user/:id", (req, res, next) => {
+  const user = users.find((item) => item.id === +req.params.id);
+  const params = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  user = { ...user, ...params };
+  users = users.map((item) => (item.id === user.id ? user : item));
+  res.status(200).json(user);
+});
+
+app.delete("/v1/user/:id", (req, res, next) => {
+  users = users.filter((item) => item.id !== +req.params.id);
+  res.status(200).json({
+    message: `Users id of ${req.params.id} has been deleted!`,
+  });
 });
 
 module.exports = app;
--- routes/games.js
+++ routes/games.js
@@ -1,14 +1,12 @@
-const { json } = require("express");
 const express = require("express");
-const users = require("../db/user.json");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const name = req.query.name || "Player";
-  const userName = users.find((item) => item.name);
+  console.log(name);
   res.render("games", {
     title: "Try Out The Games",
-    name: userName.name,
+    name: name,
   });
 });
 
